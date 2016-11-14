ae14b079b850a023bc540f0572a2cab8b29ed960
Grant <ghwilley@gmail.com>
2016-11-13 21:42:48 -0600

    created mail service

--- angular2-fundamentals/src/app/app.component.ts
+++ angular2-fundamentals/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private mail:MailService) {};
 }
