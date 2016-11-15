02d9926dfdbac5f35490ec7d1883aaf8ae867b6d
Grant <ghwilley@gmail.com>
2016-11-15 11:23:09 -0600

    listing elements of mail obj

--- angular2-fundamentals/src/app/mail.service.ts
+++ angular2-fundamentals/src/app/mail.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MailService {
 
-  message = "you've got mail lol"
+  messages = ["you've got mail lol", "bummer, shit msg", "what a day"];
 
   constructor() { }
 
